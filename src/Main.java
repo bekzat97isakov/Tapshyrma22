44869ab0ab1ea1877ff0c5686f8e4cd9153f6afe
bekzadsarybaev <bekzat97isakov>
2022-09-08 13:55:09 +0600

    commit is done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String [] args) {
+        Random random = new Random();
+        ArrayList<Integer> arr = new ArrayList<>();
+        ArrayList<Integer> arr1 = new ArrayList<>();
+        ArrayList<Integer> arr2 = new ArrayList<>();
+
+
+        for (int i = 0; i < 50; i++) {
+            int randomsan = random.nextInt(1,100);
+            arr.add(randomsan);
+            if (randomsan%2==0){
+                arr1.add(randomsan);
+            }else {
+                arr2.add(randomsan);
+            }
+
+        }
+        System.out.println("Все значение:"+arr);
+        System.out.println("Четные число:"+arr1);
+        System.out.println("Не четные число:"+arr2);
     }
+
 }
